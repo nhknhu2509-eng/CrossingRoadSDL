6428323b44c7875c5a80d5ec42e8c12bf181a2da
nhknhu2509-eng <318502145+nhknhu2509-eng@users.noreply.github.com>
2026-08-29 23:41:50 +0700

    Okay 2

--- Include/Config/GameConfig.h
+++ Include/Config/GameConfig.h
@@ -19,7 +19,7 @@ namespace Config
 
     constexpr int PLAYER_WIDTH = 70;
     constexpr int PLAYER_HEIGHT = 50;
-    constexpr int PLAYER_SPEED = 5;
+    constexpr int PLAYER_SPEED = 2;
 
     constexpr int PLAYER_START_X =
         WINDOW_WIDTH / 2
@@ -42,7 +42,7 @@ namespace Config
     // Animal
     // ==============================
 
-    constexpr int ANIMAL_SPEED = 3;
+    constexpr int ANIMAL_SPEED = 0.01;
 
 
     // ==============================
