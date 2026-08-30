56f6c4509f328b6e4a8d69df0ebb22b372a15433
nhknhu2509-eng <318502145+nhknhu2509-eng@users.noreply.github.com>
2026-08-30 22:18:07 +0700

    false 3

--- Include/Media/VideoPlayer.h
+++ Include/Media/VideoPlayer.h
@@ -1,19 +1,13 @@
 #pragma once
 
 #include <SDL.h>
-
 #include <string>
-
 
 class VideoPlayer
 {
 public:
-
     VideoPlayer();
-
-
     ~VideoPlayer();
-
 
     bool Play(
         SDL_Renderer* renderer,
