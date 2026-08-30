e15057df41afff01e26b675c55da43fb7bc5f27f
nhknhu2509-eng <318502145+nhknhu2509-eng@users.noreply.github.com>
2026-08-30 14:43:57 +0700

    okay 3

--- Src/World/Map.cpp
+++ Src/World/Map.cpp
@@ -1,32 +1,76 @@
 #include "World/Map.h"
+
 #include "Graphics/TextureManager.h"
+
+
+// ==================================================
+// CONSTRUCTOR
+// ==================================================
 
 Map::Map()
 {
 }
+
+
+// ==================================================
+// UPDATE
+// ==================================================
 
 void Map::Update()
 {
     vehicleManager.Update();
 }
 
+
+// ==================================================
+// DRAW
+// ==================================================
+
 void Map::Draw(
     SDL_Renderer* renderer,
     TextureManager& textureManager)
 {
-    goal.Draw(renderer);
+    goal.Draw(
+        renderer);
+
 
     vehicleManager.Draw(
         renderer,
         textureManager);
 }
 
-Goal& Map::GetGoal()
+
+// ==================================================
+// GET GOAL
+// ==================================================
+
+Goal&
+Map::GetGoal()
 {
     return goal;
 }
 
-VehicleManager& Map::GetVehicleManager()
+
+const Goal&
+Map::GetGoal() const
+{
+    return goal;
+}
+
+
+// ==================================================
+// GET VEHICLE MANAGER
+// ==================================================
+
+VehicleManager&
+Map::GetVehicleManager()
+{
+    return vehicleManager;
+}
+
+
+const VehicleManager&
+Map::GetVehicleManager() const
 {
     return vehicleManager;
 }
